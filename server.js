528e2995d69886398b9f14cab4aea4bda2800473
Sasha <lyshirokov613@gmail.com>
2017-06-11 22:34:32 +0300

    keep fixing server

--- server.js
+++ server.js
@@ -28,3 +28,23 @@ app.listen(PORT, function() {
 // app.listen(3000, function() {
 // 	console.log("Express server is up on port 3000");
 // });
+
+// var express = require("express");
+//
+// // Create our app
+// var app = express();
+// const PORT = process.env.PORT || 3000;
+//
+// app.use(function(req, res, next) {
+// 	if (req.headers["x-forwarded-proto"] === "https") {
+// 		res.redirect("http://" + req.hostname + req.url);
+// 	} else {
+// 		next();
+// 	}
+// });
+//
+// app.use(express.static("docs"));
+//
+// app.listen(PORT, function() {
+// 	console.log("Express server is up on port " + PORT);
+// });
