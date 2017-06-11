26fbff224faade23624da1de166eef8a7d657753
Sasha <lyshirokov613@gmail.com>
2017-06-11 22:38:07 +0300

    server

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-var express = require("express");
-
-// Create our app
-var app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(function(req, res, next) {
-	if (req.headers['x-forward-proto'] === 'http') {
-		next();
-	} else {
-		res.redirect('http://' + req.hostname + req.url);
-	}
-});
-
-app.use(express.static("docs"));
-
-app.listen(PORT, function() {
-	console.log("Express server is up on port " + PORT);
-});
+// var express = require("express");
+//
+// // Create our app
+// var app = express();
+// const PORT = process.env.PORT || 3000;
+//
+// app.use(function(req, res, next) {
+// 	if (req.headers['x-forward-proto'] === 'http') {
+// 		next();
+// 	} else {
+// 		res.redirect('http://' + req.hostname + req.url);
+// 	}
+// });
+//
+// app.use(express.static("docs"));
+//
+// app.listen(PORT, function() {
+// 	console.log("Express server is up on port " + PORT);
+// });
 
 // var express = require("express");
 //
@@ -29,22 +29,22 @@ app.listen(PORT, function() {
 // 	console.log("Express server is up on port 3000");
 // });
 
-// var express = require("express");
-//
-// // Create our app
-// var app = express();
-// const PORT = process.env.PORT || 3000;
-//
-// app.use(function(req, res, next) {
-// 	if (req.headers["x-forwarded-proto"] === "https") {
-// 		res.redirect("http://" + req.hostname + req.url);
-// 	} else {
-// 		next();
-// 	}
-// });
-//
-// app.use(express.static("docs"));
-//
-// app.listen(PORT, function() {
-// 	console.log("Express server is up on port " + PORT);
-// });
+var express = require("express");
+
+// Create our app
+var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function(req, res, next) {
+	if (req.headers["x-forwarded-proto"] === "https") {
+		res.redirect("http://" + req.hostname + req.url);
+	} else {
+		next();
+	}
+});
+
+app.use(express.static("docs"));
+
+app.listen(PORT, function() {
+	console.log("Express server is up on port " + PORT);
+});
